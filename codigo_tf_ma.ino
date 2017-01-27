35f4ca621325e04e226e8458a6ec7ffd7c356675
beatrizleitenegrao <beatrizleitenegrao@gmail.com>
2017-01-27 14:45:51 -0200

    Código Arduino
    Upload do código inicial para o arduíno

---
+++ codigo_tf_ma.ino
@@ -0,0 +1,37 @@
+
+
+int sensorTemperatura = A0;
+int sinalRele = 4;
+
+
+
+
+void setup() {
+ pinMode (sinalRele, OUTPUT);
+ 
+ Serial.begin(9600);
+ 
+ }
+
+void loop() {
+  int leituraSensor = analogRead(sensorTemperatura);
+  
+  float volt = (leituraSensor*5.0);
+  volt/=1024.0;
+  
+  float temperatura=(volt-0.5)*10; 
+  
+  Serial.print(temperatura); Serial.println(" celsius");
+  
+  
+  if(temperatura < 27){
+    digitalWrite (sinalRele, HIGH); 
+    Serial.println("on");
+  }
+  else{
+    digitalWrite (sinalRele,  LOW);
+    Serial.println("off");
+  }
+
+
+}
