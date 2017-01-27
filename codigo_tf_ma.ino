7108ea974bb2dfaa5b7921e1481c5cd83176ccf9
beatrizleitenegrao <beatrizleitenegrao@gmail.com>
2017-01-27 14:59:29 -0200

    Comentários
    - Correção da porta do Relé que no esquemático tínhamos definido como 4
    - Adição de comentários para clareza do código

--- codigo_tf_ma.ino
+++ codigo_tf_ma.ino
@@ -1,34 +1,42 @@
 
-
+//declarações
 int sensorTemperatura = A0;
-int sinalRele = 4;
-
-
+int sinalRele = 10;
 
 
 void setup() {
+ //declaração de pinos
  pinMode (sinalRele, OUTPUT);
  
- Serial.begin(9600);
+ Serial.begin(9600); //Inicializa a conexão serial com o computador
  
  }
 
 void loop() {
-  int leituraSensor = analogRead(sensorTemperatura);
+//leitura do sensor de temperatura
+  int leituraSensor = analogRead (sensorTemperatura);
   
-  float volt = (leituraSensor*5.0);
-  volt/=1024.0;
+  //corvertendo a leitura para voltagem
+  float volt = leituraSensor * 5.0;
+  volt /= 1024.0;
   
-  float temperatura=(volt-0.5)*10; 
+  //convertendo a temperatura de fahrenheit para Celsius
+  float temperatura = (volt - 0.5) * 10; 
   
   Serial.print(temperatura); Serial.println(" celsius");
   
+  //controlando a temperatura
   
-  if(temperatura < 27){
+  if (temperatura < 27)
+  {
+    //se a temperatura estiver abaixo de 27 graus a lâmpada é ligada
     digitalWrite (sinalRele, HIGH); 
     Serial.println("on");
   }
-  else{
+  
+  else 
+  {
+    //caso a temperatura estiver acima de 27 graus a lâmpada é desligada
     digitalWrite (sinalRele,  LOW);
     Serial.println("off");
   }
