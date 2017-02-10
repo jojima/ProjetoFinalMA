25eda1327857d484b5af5b469a874653c952658e
jojima <fabio_jojima@hotmail.com>
2017-02-10 15:32:25 -0200

    Audio player
    Adicionado parte de reprodução de arquivos de áudio via cartão SD utilizando as libraries SD.h e TMRpcm.h, a segunda está disponível na própria IDE para download.

--- codigo_tf_ma.ino
+++ codigo_tf_ma.ino
@@ -1,25 +1,43 @@
+#include <SD.h>
+#include <TMRpcm.h>
+#include <SPI.h>
 
 //declarações
 int sensorTemperatura = A0;
 int sinalRele = 10;
-
+TMRpcm tmrpcm; //estrutura que contem funcoes para reproduzir o som
 
 void setup() {
  //declaração de pinos
  pinMode (sinalRele, OUTPUT);
- 
+ tmrpcm.speakerPin = 9; //5,6,11 ou 46 on Mega, 9 no Uno, Nano, etc
+ //pode ser utilizado outra saida como auxiliar para o som
  Serial.begin(9600); //Inicializa a conexão serial com o computador
- 
+  if (!SD.begin(4)) { //para o nano utiliza o 4
+    Serial.println(" failed!"); //caso nao seja possivel ler o cartao sd
+    while(true);
+  }
  }
-
+ 
 void loop() {
+	
 //leitura do sensor de temperatura
   int leituraSensor = analogRead (sensorTemperatura);
-  
   //corvertendo a leitura para voltagem
   float volt = leituraSensor * 5.0;
   volt /= 1024.0;
-  
+
+  // File myFile = SD.open("coracao.wav");
+  // if (!myFile) {
+    //Erro se nao abrir o arquivo .wav
+    // Serial.println("Nao foi possivel abrir coracao.wav");
+    //fica em idle
+    // while (true);
+  // }
+
+  // const int S = 1024; // Tamanho (size) do buffer a ser utilizado
+  // short buffer[S];
+  tmrpcm.play("heartbeats.wav"); //reproduz o arquivo "heartbeats.wav"
   //convertendo a temperatura de fahrenheit para Celsius
   float temperatura = (volt - 0.5) * 10; 
   
@@ -40,6 +58,6 @@ void loop() {
     digitalWrite (sinalRele,  LOW);
     Serial.println("off");
   }
-
+  
 
 }
